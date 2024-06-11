c2e7b9145fa5e4c546470362b4a34a91d6936353
f4zker121 <utkonos322@gmail.com>
2024-06-11 18:08:08 +0300

    .

---
+++ pkg/service/auth.go
@@ -0,0 +1,79 @@
+package service
+
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	todo "myRest"
+	"myRest/pkg/repository"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	salt       = "kljhafsedklhjcvxzwmmqe"
+	signingKey = ";jlkxdcv;lkjdfs"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UsedId int `json:"user_id"`
+}
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) ParseToken(acsessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(acsessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UsedId, nil
+}
